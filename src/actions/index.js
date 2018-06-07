65cfca77701db561ef918a76b4d1951eb0e7fd1d
AJ Call <AJCall710@gmail.com>
2018-06-07 13:51:11 -0600

    added signin redirect to newsletter

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
+import history from '../history';
+
 const ROOT_URL = 'https://bottega-property-management.herokuapp.com'
 
 export function signinUser({email, password}) {
     return function(dispatch) {
         axios.post(`${ROOT_URL}/signin`, { email, password} )
         .then(response => {
-            console.log(response.data.token)
+            console.log(response.data.token);
+
+            history.push('./newsletter');
         })
         .catch(error => {
             consoel.log(error);
