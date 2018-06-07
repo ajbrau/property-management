25afd48e592e5185b5e13e1ba5a010ad4aa1bbc6
AJ Call <AJCall710@gmail.com>
2018-06-07 12:23:09 -0600

    added form fields

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class Signin extends Component {
+
+    renderInput(field) {
+        return <input className="form-control" {...field.input} type="field.type"/>
+    }
+
+    handleFormSubmit = function() {
+
+    }
+
     render() {
+        const { handleSubmit } = this.props;
         return (
-            <div>
-                sign in form
-            </div>
+            <form onSubmit={this.handleFormSubmit.bind(this)}>
+                <label htmlFor="email">Email</label>
+                <Field name="email" component={this.renderInput} type="text"/>
+                <label htmlFor="email">Email</label>
+                <Field name="password" component={this.renderInput} type="password"/>
+
+                <button sction="submit" clasName="btn btn-primary">Sign In</button>
+            </form>
         );
     }
 }
 
-export default Signin;
+export default reduxForm({form: "signin"})(Signin);
--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class Signup extends Component {
+
+    renderInput(field) {
+        return <input className="form-control" {...field.input} type="field.type"/>
+    }
     render() {
         return (
             <div>
-                sign up form
+                <label htmlFor="email">Email</label>
+                <Field name="email" component={this.renderInput} type="text"/>
+                <label htmlFor="email">Email</label>
+                <Field name="password" component={this.renderInput} type="password"/>
             </div>
         );
     }
 }
 
-export default Signup;
+export default reduxForm({form: "signup"})(Signup);
